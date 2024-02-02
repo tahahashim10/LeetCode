5d5afcd1fc01637dacb1faa17899d401fc5dc35e
Taha Hashim <128276249+tahahashim10@users.noreply.github.com>
2024-02-02 13:04:18 -0500

    Time: 209 ms (20.33%) | Memory: 17.6 MB (95.60%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -2,15 +2,27 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2): return False
 
-        d1 = defaultdict(int)
-        d2 = defaultdict(int)
+        # d1 = defaultdict(int)
+        # d2 = defaultdict(int)
+
+        # for c1 in word1:
+        #     d1[c1] += 1
+        # for c2 in word2:
+        #     d2[c2] += 1
+
+        d1 = [0] * 26
+        d2 = [0] * 26
 
         for c1 in word1:
-            d1[c1] += 1
+            d1[ord(c1) - ord('a')] += 1
         for c2 in word2:
-            d2[c2] += 1
+            d2[ord(c2) - ord('a')] += 1
+
+        return sorted(d1) == sorted(d2) and set(word1) == set(word2)
+
+        
             
         #the keys should be the same since we sorted the strings
         #the sorted values should match the keys of both strings
-        return d1.keys() == d2.keys() and sorted(d1.values()) == sorted(d2.values())
+        # return d1.keys() == d2.keys() and sorted(d1.values()) == sorted(d2.values())
         
