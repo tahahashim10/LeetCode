2d9ae80c5032a489af857256fff927f9279f2729
Taha Hashim <128276249+tahahashim10@users.noreply.github.com>
2024-02-02 13:01:53 -0500

    Time: 196 ms (31.76%) | Memory: 17.3 MB (99.71%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,10 +5,12 @@ class Solution:
         d1 = defaultdict(int)
         d2 = defaultdict(int)
 
-        for c1, c2 in zip(word1, word2):
+        for c1 in word1:
             d1[c1] += 1
+        for c2 in word2:
             d2[c2] += 1
             
         #the keys should be the same since we sorted the strings
         #the sorted values should match the keys of both strings
         return d1.keys() == d2.keys() and sorted(d1.values()) == sorted(d2.values())
+        
